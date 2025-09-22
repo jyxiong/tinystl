0897ec126cba2cb89db24eec30c73c099f6222fe
jyxiong <jingyu.xiong@foxmail.com>
2025-09-22 19:02:33 +0800

    feat: add reference type traits

---
+++ source/tinystl/type_traits/primary_type_categories/is_rvalue_reference.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "tinystl/type_traits/helper_classes/integral_constant.h"
+
+namespace tinystl {
+template<class T>
+struct is_rvalue_reference : false_type {};
+template<class T>
+struct is_rvalue_reference<T&&> : true_type {};
+
+template <class T>
+inline constexpr bool is_rvalue_reference_v = is_rvalue_reference<T>::value;
+}
