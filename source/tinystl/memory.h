347267f71de0616a0d82cf792f89f7b8e62a909a
jyxiong <jingyu.xiong@foxmail.com>
2025-09-24 09:56:55 +0800

    refactor: use std insteadof tinystl (#26)

--- source/tinystl/memory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <tinystl/memory/addressof.h>
-#include <tinystl/memory/allocator.h>
-#include <tinystl/memory/allocator_traits.h>
-#include <tinystl/memory/construct.h>
-#include <tinystl/memory/destroy.h>
-#include <tinystl/memory/pointer_traits.h>
-
